10c3a6f4d7fb9ae2ab998d2abf6f36acb15e57f4
ntzwilly <ntzwilly@gmail.com>
2022-11-03 20:38:05 +0200

    Described class

---
+++ spec/described_class_spec.rb
@@ -0,0 +1,21 @@
+class Royal
+
+  attr_reader :name
+
+  def initialize(name) 
+    @name = name
+  end
+
+end
+
+RSpec.describe Royal do
+
+  subject { described_class.new('Boris') }
+  let(:louis) { described_class.new('Louis') }
+
+  it 'represent a great person' do
+    expect(subject.name).to eq('Boris')
+    expect(louis.name).to eq('Louis')
+  end
+
+end
