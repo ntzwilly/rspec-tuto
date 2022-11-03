c9eafc64fbee445f458971f4ebb3a5d1b424e67c
ntzwilly <ntzwilly@gmail.com>
2022-11-03 21:22:56 +0200

    Shared context

---
+++ spec/shared_context_spec.rb
@@ -0,0 +1,43 @@
+RSpec.shared_context 'common' do
+
+  before do
+    @foods = []
+  end
+
+  def some_helper_method
+    5
+  end
+
+  let(:some_variables) { [1,2,3] }
+
+end
+
+RSpec.describe 'first example group' do
+
+  include_context 'common'
+
+  it 'can use outside instance variable' do
+    expect(@foods.length).to eq(0)
+    @foods << 'Sushi'
+    expect(@foods.length).to eq(1)
+  end
+
+  it 'can reuse outside instance variabe in different examples' do
+    expect(@foods.length).to eq(0)
+  end
+
+  it 'can use shared helper methods' do
+    expect(some_helper_method).to eq(5)
+  end
+
+end
+
+RSpec.describe 'second example in different file' do
+
+  include_context 'common'
+
+  it 'can use shared let variable' do
+    expect(some_variables).to eq([1, 2, 3])
+  end
+
+end
