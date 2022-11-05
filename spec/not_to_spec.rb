a60486861cc1cf57904dee049b759fb234790502
ntzwilly <ntzwilly@gmail.com>
2022-11-05 16:24:27 +0200

    not_to spec

---
+++ spec/not_to_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'not_to method' do
+
+  it 'checks for the inverse of a matcher' do
+    expect(5).not_to eq(10)
+    expect([1, 2, 3]).not_to equal([1, 2, 3])
+    expect(10).not_to be_odd
+    expect([1, 2, 3]).not_to be_empty
+    expect(nil).not_to be_truthy
+    expect('philadelphia').not_to start_with('hil')
+    expect('philadelphia').not_to end_with('hil')
+    expect(5).not_to respond_to(:length)
+    expect([:a, :b, :c]).not_to include(:d)
+    expect { 11/3 }.not_to raise_error
+  end
+
+end
